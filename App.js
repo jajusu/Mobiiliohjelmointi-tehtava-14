68b14a8ae165fcca494fc4a0efd5c04b4a35de24
J <j.suomaki@gmail.com>
2022-03-14 14:43:47 +0200

    Valmis?

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import * as React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import { useState} from 'react';
+import { View, StyleSheet, Button, TextInput } from 'react-native';
 import * as Speech from 'expo-speech';
 
 export default function App() {
+  const [syote,setSyote]=useState('');
+
   const speak = () => {
-    const thingToSay = 'Houston we have a problem';
+    const thingToSay = syote;
+    console.log("syote ",syote)
     Speech.speak(thingToSay);
   };
 
   return (
     <View style={styles.container}>
+        <TextInput style={styles.inputti}
+          autoFocus={true}
+          onChangeText={text => setSyote(text)}
+        />
       <Button title="Press to hear some words" onPress={speak} />
     </View>
   );
@@ -22,4 +30,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputti:{
+    fontSize: 18, 
+    width: 210,
+    borderColor:'black',
+    borderWidth:2,
+
+   }
 });
